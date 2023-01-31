8fbe899662ae74bcee82c96736cac6512f211e16
Winter <thetruewinter@gmail.com>
2023-01-31 10:11:05 +0000

    Created user model

---
+++ src/main/java/com/tp32/ecommerceplatform/model/User.java
@@ -0,0 +1,64 @@
+package com.tp32.ecommerceplatform.model;
+
+import jakarta.persistence.*;
+
+/**
+ * A model which holds the application data of a particular User, used to be displayed within a view.
+ */
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 60, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 60)
+    private String password;
+
+    @Column(name = "first_name", nullable = false, length = 25)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 25)
+    private String lastName;
+
+    public Long getID() {
+        return this.id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+}
