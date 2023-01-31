c24a4c5b9c67a4a0edbd6b2dcce76e9496a13bc9
Winter <thetruewinter@gmail.com>
2023-01-31 10:08:06 +0000

    Refactored controllers directory

--- src/main/java/com/tp32/ecommerceplatform/controller/IndexController.java
+++ src/main/java/com/tp32/ecommerceplatform/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.tp32.ecommerceplatform.controllers;
+package com.tp32.ecommerceplatform.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     /**
-     * Routes all requests to "/" to the static index.html page
+     * Routes all requests to "/" to the template index.html page
      * @return the view that is to be resolved and displayed
      */
     @RequestMapping("/")
