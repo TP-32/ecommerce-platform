c6dc95789387ef78e24aa6939c617d70eebabafd
Winter <thetruewinter@gmail.com>
2023-02-20 22:59:34 +0000

    Handles the transfer of the Jwt

---
+++ src/main/java/com/tp32/ecommerceplatform/dto/JwtResponse.java
@@ -0,0 +1,24 @@
+package com.tp32.ecommerceplatform.dto;
+
+/**
+ * Data Transfer Object which will send information from the front-end to the back-end.
+ * For this Object, it is handling the Token that determines who the User is.
+ */
+public class JwtResponse {
+    
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+    public String getToken() {
+        return this.accessToken;
+    }
+
+    public String getTokenType() {
+        return this.tokenType;
+    }
+
+    public void setToken(String token) {
+        this.accessToken = token;
+    }
+
+}
