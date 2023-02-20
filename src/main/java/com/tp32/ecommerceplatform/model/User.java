13cf83446eaa655e10f36f0e021aaf195a9ef49b
Winter <thetruewinter@gmail.com>
2023-02-20 22:48:55 +0000

    Database model of Users

--- src/main/java/com/tp32/ecommerceplatform/model/User.java
+++ src/main/java/com/tp32/ecommerceplatform/model/User.java
@@ -3,10 +3,11 @@ package com.tp32.ecommerceplatform.model;
 import jakarta.persistence.*;
 
 /**
- * A model which holds the application data of a particular User, used to be displayed within a view.
+ * A model which holds the application data of a particular User, used to be
+ * displayed within a view.
  */
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
 
     @Id
@@ -25,6 +26,30 @@ public class User {
     @Column(name = "last_name", nullable = false, length = 25)
     private String lastName;
 
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles", joinColumns = {
+            @JoinColumn(name = "user_id", referencedColumnName = "id") }, inverseJoinColumns = {
+                    @JoinColumn(name = "role_id", referencedColumnName = "id") })
+    private Role role;
+
+    /*
+     * Constructor to initialise every value of the User.
+     */
+    public User(String firstName, String lastName, String email, String password, Role role) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+    }
+
+    /*
+     * Empty constructor is used here to create the Object without initialising its values immediately.
+     */
+    public User() {
+    }
+    
+    // Getters
     public Long getID() {
         return this.id;
     }
@@ -45,6 +70,11 @@ public class User {
         return this.lastName;
     }
 
+    public Role getRole() {
+        return this.role;
+    }
+
+    // Setters
     public void setEmail(String email) {
         this.email = email;
     }
@@ -60,5 +90,9 @@ public class User {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
 }
