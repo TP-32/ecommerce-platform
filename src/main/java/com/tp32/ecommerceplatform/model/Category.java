555e0f728702bb91f5f329b2cb04ac43a47f304a
Winter <thetruewinter@gmail.com>
2023-03-23 18:20:32 +0000

    Removed unnecessary methods and renamed name field

--- src/main/java/com/tp32/ecommerceplatform/model/Category.java
+++ src/main/java/com/tp32/ecommerceplatform/model/Category.java
@@ -17,7 +17,7 @@ public class Category {
     private Long id;
 
     @Column(name = "category_name", nullable = false, length = 25)
-    private String categoryName;
+    private String name;
 
     @Column(name="description", length = 255)
     private String description;
@@ -33,27 +33,19 @@ public class Category {
     }
 
     public String getName() {
-        return this.categoryName;
+        return this.name;
     }
 
     public String getDescription() {
         return this.description;
     }
 
-    // public List<Product> getProducts() {
-    //     return this.products;
-    // }
-
     // Setters
-    public void setName(String categoryName) {
-        this.categoryName = categoryName;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
-
-    // public void setProducts(List<Product> products) {
-    //     this.products = products;
-    // }
 }
