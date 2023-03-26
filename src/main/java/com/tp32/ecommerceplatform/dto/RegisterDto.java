cb94a158ab9c39ebfe1e69463dc052fd050d5d6b
Winter <thetruewinter@gmail.com>
2023-03-27 00:55:17 +0100

    Added validation to user inputs

--- src/main/java/com/tp32/ecommerceplatform/dto/RegisterDto.java
+++ src/main/java/com/tp32/ecommerceplatform/dto/RegisterDto.java
@@ -1,13 +1,28 @@
 package com.tp32.ecommerceplatform.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 /**
  * Data Transfer Object which will send information from the front-end to the back-end.
  */
 public class RegisterDto {
     
+    @Size(min = 1, message = "First Name length must be greater than 1.")
+    @Size(max = 50, message = "First Name length must be less than 50.")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "First Name must only contain upper or lowercase letters.")
     private String firstName;
+    
+    @Size(min = 1, message = "Last Name length must be greater than 1.")
+    @Size(max = 50, message = "Last Name length must be less than 50.")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Last Name must only contain upper or lowercase letters.")
     private String lastName;
+
+    @Email(message = "Must be a valid email.")
     private String email;
+
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "Password must be at least 8 characters and contain at least 1 capital letter, 1 number and 1 special character.")
     private String password;
 
     public String getFirstName() {
--- src/main/java/com/tp32/ecommerceplatform/dto/UpdateUserDto.java
+++ src/main/java/com/tp32/ecommerceplatform/dto/UpdateUserDto.java
@@ -1,15 +1,43 @@
 package com.tp32.ecommerceplatform.dto;
 
+import com.tp32.ecommerceplatform.model.User;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 /**
  * Data Transfer Object which will send information from the front-end to the back-end.
  */
 public class UpdateUserDto {
     
+    @Size(min = 1, message = "First Name length must be greater than 1.")
+    @Size(max = 50, message = "First Name length must be less than 50.")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "First Name must only contain upper or lowercase letters.")
     private String firstName;
+
+    @Size(min = 1, message = "Last Name length must be greater than 1.")
+    @Size(max = 50, message = "Last Name length must be less than 50.")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Last Name must only contain upper or lowercase letters.")
     private String lastName;
+
+    @Email(message = "Must be a valid email.")
     private String email;
+
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}|$", message = "Password must be at least 8 characters and contain at least 1 capital letter, 1 number and 1 special character.")
     private String password;
+
     private String role;
+
+    public UpdateUserDto() {
+    }
+
+    public UpdateUserDto(User user) {
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+        this.role = user.getRole().getName();
+    }
 
     public String getFirstName() {
         return this.firstName;
