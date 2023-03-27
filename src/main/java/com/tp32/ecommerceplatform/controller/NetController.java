04e255abcc555421f57c427889b72d524c721300
Winter <thetruewinter@gmail.com>
2023-03-27 15:53:08 +0100

    Created checkout request to input address details

--- src/main/java/com/tp32/ecommerceplatform/controller/NetController.java
+++ src/main/java/com/tp32/ecommerceplatform/controller/NetController.java
@@ -1,19 +1,26 @@
 package com.tp32.ecommerceplatform.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.tp32.ecommerceplatform.dto.AddressDto;
 import com.tp32.ecommerceplatform.dto.NetDto;
 import com.tp32.ecommerceplatform.model.Net;
 import com.tp32.ecommerceplatform.model.User;
 import com.tp32.ecommerceplatform.service.NetItemService;
 import com.tp32.ecommerceplatform.service.NetService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class NetController {
@@ -53,19 +60,36 @@ public class NetController {
         return model;
     }
 
-    @GetMapping("/net/submit")
-    public ModelAndView submitNet() {
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    @GetMapping("/net/checkout")
+    public String checkout(Model model) {
+        model.addAttribute("addressDto", new AddressDto());
+        model.addAttribute("countries", getCountries());
+        return "checkout.html";
+    }
 
-        Net net = netService.findByUser(user);
-
-        if (net == null) {
-            return new ModelAndView("redirect:/categories");
+    @PostMapping("/net/submit")
+    public String submitNet(@Valid @ModelAttribute("addressDto") AddressDto addressDto, BindingResult result, Model model) {
+        if (!result.hasErrors()) {
+            User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            Net net = netService.findByUser(user);
+            if (net == null) return "categories.html";
+            netService.close(net.getID());
+            return "redirect:/orders";
         }
-        netService.close(net.getID()); 
 
-        ModelAndView model = new ModelAndView("redirect:/orders");
-        return model;
+        model.addAttribute("countries", getCountries());
+        return "checkout.html";
+    }
+
+    private List<String> getCountries() {
+        List<String> countries = new ArrayList<>();
+        countries.add("Spain");
+        countries.add("United Kingdom");
+        countries.add("Italy");
+        countries.add("France");
+        countries.add("Germany");
+        countries.add("Belgium");
+        return countries;
     }
 
 }
